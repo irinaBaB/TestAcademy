268b64832fbe20562b92267b73411983e35a7536
Irina Babicheva <irina.babicheva@myob.com>
2019-03-27 14:07:44 +1100

    parametrazing test

--- test_flight_booking.py
+++ test_flight_booking.py
@@ -6,77 +6,118 @@ import unittest
 
 class TestFlightBooking(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path='drivers/chromedriver')
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Chrome(executable_path='drivers/chromedriver')
+        self.wd.implicitly_wait(30)
 
     def test_flight_booking(self):
-        driver = self.driver
-        driver.get("http://newtours.demoaut.com/mercurysignon.php")
-        driver.find_element_by_name("userName").click()
-        driver.find_element_by_name("userName").clear()
-        driver.find_element_by_name("userName").send_keys("mercury")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("mercury")
-        driver.find_element_by_name("login").click()
-        driver.find_element_by_name("tripType").click()
-        driver.find_element_by_xpath("//select[@name='passCount']").click()
-        driver.find_element_by_name("passCount").click()
-        driver.find_element_by_name("fromPort").click()
-        Select(driver.find_element_by_name("fromPort")).select_by_visible_text("London")
-        driver.find_element_by_name("fromMonth").click()
-        Select(driver.find_element_by_name("fromMonth")).select_by_visible_text("April")
-        driver.find_element_by_name("fromDay").click()
-        Select(driver.find_element_by_name("fromDay")).select_by_visible_text("7")
-        driver.find_element_by_name("toPort").click()
-        Select(driver.find_element_by_name("toPort")).select_by_visible_text("New York")
-        driver.find_element_by_name("toMonth").click()
-        Select(driver.find_element_by_name("toMonth")).select_by_visible_text("April")
-        driver.find_element_by_name("toDay").click()
-        Select(driver.find_element_by_name("toDay")).select_by_visible_text("20")
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Airline:'])[1]/preceding::input[2]").click()
-        driver.find_element_by_name("airline").click()
-        Select(driver.find_element_by_name("airline")).select_by_visible_text("Blue Skies Airlines")
-        driver.find_element_by_name("findFlights").click()
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Blue Skies Airlines 361'])[1]/preceding::input[1]").click()
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Blue Skies Airlines 631'])[1]/preceding::input[1]").click()
-        driver.find_element_by_name("reserveFlights").click()
-        driver.find_element_by_name("passFirst0").click()
-        driver.find_element_by_name("passFirst0").clear()
-        driver.find_element_by_name("passFirst0").send_keys("helen")
-        driver.find_element_by_name("passLast0").clear()
-        driver.find_element_by_name("passLast0").send_keys("conor")
-        driver.find_element_by_name("pass.0.meal").click()
-        Select(driver.find_element_by_name("pass.0.meal")).select_by_visible_text("Vegetarian")
-        driver.find_element_by_name("creditCard").click()
-        Select(driver.find_element_by_name("creditCard")).select_by_visible_text("Visa")
-        driver.find_element_by_name("creditnumber").click()
-        driver.find_element_by_name("creditnumber").clear()
-        driver.find_element_by_name("creditnumber").send_keys("4678576578")
-        driver.find_element_by_name("cc_exp_dt_mn").click()
-        Select(driver.find_element_by_name("cc_exp_dt_mn")).select_by_visible_text("07")
-        driver.find_element_by_name("cc_exp_dt_yr").click()
-        Select(driver.find_element_by_name("cc_exp_dt_yr")).select_by_visible_text("2010")
-        driver.find_element_by_name("cc_frst_name").click()
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Billing Address'])[1]/following::tr[1]").click()
-        driver.find_element_by_name("billAddress1").clear()
-        driver.find_element_by_name("billAddress1").send_keys("12 Normanbyroad")
-        driver.find_element_by_name("billCity").click()
-        driver.find_element_by_name("billCity").clear()
-        driver.find_element_by_name("billCity").send_keys("auckland")
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='State/Province:'])[1]/following::td[1]").click()
-        driver.find_element_by_name("billState").click()
-        driver.find_element_by_name("billState").clear()
-        driver.find_element_by_name("billState").send_keys("")
-        driver.find_element_by_name("billZip").click()
-        driver.find_element_by_name("billZip").clear()
-        driver.find_element_by_name("billZip").send_keys("3192")
-        driver.find_element_by_name("billCountry").click()
-        Select(driver.find_element_by_name("billCountry")).select_by_visible_text("NEW ZEALAND")
-        driver.find_element_by_name("buyFlights").click()
-        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='$588 USD'])[1]/following::img[4]").click()
-    
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="mercury", password="mercury")
+        self.flight_booking(wd, from_port="London",
+                                departure_month="April",
+                                departure_date="7",
+                                to_port="New York",
+                                return_month= "April",
+                                return_date= "20")
+        self.flight_purchase(wd, pas_first_name="helen",
+                                 pas_last_name="conor",
+                                 meal_option="Vegetarian",
+                                 cc_type="Visa",
+                                 cc_number="4678576578",
+                                 cc_exp_month="07",
+                                 cc_exp_year="2010",
+                                 bill_address="12 Normanbyroad",
+                                 city="auckland",
+                                 zip_code="3192",
+                                 country="NEW ZEALAND")
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='$588 USD'])[1]/following::img[4]").click()
+
+    def flight_purchase(self, wd, pas_first_name, pas_last_name, meal_option, cc_type, cc_number, cc_exp_month,
+                        cc_exp_year, bill_address, city, zip_code, country):
+        wd.find_element_by_name("reserveFlights").click()
+        wd.find_element_by_name("passFirst0").click()
+        wd.find_element_by_name("passFirst0").clear()
+        wd.find_element_by_name("passFirst0").send_keys(pas_first_name)
+        wd.find_element_by_name("passLast0").clear()
+        wd.find_element_by_name("passLast0").send_keys(pas_last_name)
+        wd.find_element_by_name("pass.0.meal").click()
+        Select(wd.find_element_by_name("pass.0.meal")).select_by_visible_text(meal_option)
+        wd.find_element_by_name("creditCard").click()
+        Select(wd.find_element_by_name("creditCard")).select_by_visible_text(cc_type)
+        wd.find_element_by_name("creditnumber").click()
+        wd.find_element_by_name("creditnumber").clear()
+        wd.find_element_by_name("creditnumber").send_keys(cc_number)
+        wd.find_element_by_name("cc_exp_dt_mn").click()
+        Select(wd.find_element_by_name("cc_exp_dt_mn")).select_by_visible_text(cc_exp_month)
+        wd.find_element_by_name("cc_exp_dt_yr").click()
+        Select(wd.find_element_by_name("cc_exp_dt_yr")).select_by_visible_text(cc_exp_year)
+        wd.find_element_by_name("cc_frst_name").click()
+        # billing address
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Billing Address'])[1]/following::tr[1]").click()
+        wd.find_element_by_name("billAddress1").clear()
+        wd.find_element_by_name("billAddress1").send_keys(bill_address)
+        wd.find_element_by_name("billCity").click()
+        wd.find_element_by_name("billCity").clear()
+        wd.find_element_by_name("billCity").send_keys(city)
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='State/Province:'])[1]/following::td[1]").click()
+        wd.find_element_by_name("billState").click()
+        wd.find_element_by_name("billState").clear()
+        wd.find_element_by_name("billState").send_keys("")
+        wd.find_element_by_name("billZip").click()
+        wd.find_element_by_name("billZip").clear()
+        wd.find_element_by_name("billZip").send_keys(zip_code)
+        wd.find_element_by_name("billCountry").click()
+        Select(wd.find_element_by_name("billCountry")).select_by_visible_text(country)
+        wd.find_element_by_name("buyFlights").click()
+
+    def flight_booking(self, wd, from_port, departure_month, departure_date, to_port, return_month, return_date):
+        wd.find_element_by_name("tripType").click()
+        wd.find_element_by_xpath("//select[@name='passCount']").click()
+        wd.find_element_by_name("passCount").click()
+        # from port
+        wd.find_element_by_name("fromPort").click()
+        Select(wd.find_element_by_name("fromPort")).select_by_visible_text(from_port)
+        wd.find_element_by_name("fromMonth").click()
+        Select(wd.find_element_by_name("fromMonth")).select_by_visible_text(departure_month)
+        wd.find_element_by_name("fromDay").click()
+        Select(wd.find_element_by_name("fromDay")).select_by_visible_text(departure_date)
+        wd.find_element_by_name("toPort").click()
+        # to destionation
+        Select(wd.find_element_by_name("toPort")).select_by_visible_text(to_port)
+        wd.find_element_by_name("toMonth").click()
+        Select(wd.find_element_by_name("toMonth")).select_by_visible_text(return_month)
+        wd.find_element_by_name("toDay").click()
+        Select(wd.find_element_by_name("toDay")).select_by_visible_text(return_date)
+        # select airline
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Airline:'])[1]/preceding::input[2]").click()
+        wd.find_element_by_name("airline").click()
+        Select(wd.find_element_by_name("airline")).select_by_visible_text("Blue Skies Airlines")
+        wd.find_element_by_name("findFlights").click()
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Blue Skies Airlines 361'])[1]/preceding::input[1]").click()
+        wd.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Blue Skies Airlines 631'])[1]/preceding::input[1]").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("userName").click()
+        wd.find_element_by_name("userName").clear()
+        wd.find_element_by_name("userName").send_keys(username)
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys(password)
+        wd.find_element_by_name("login").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://newtours.demoaut.com/mercurysignon.php")
+
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
 
 
 if __name__ == "__main__":
